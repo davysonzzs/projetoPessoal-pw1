3c65890fcd955d3c9d9a34e6b16b3e371737e4fb
davyson <davyssonlevi2.0@gmail.com>
2026-03-24 11:24:33 -0300

    começo a escrever estages

---
+++ src/js/criadorGuia.js
@@ -0,0 +1,29 @@
+//variveis onde ficam stages
+const cursoCss = {
+    "stage 1: Introdução": [
+    `CSS é a linguagem que usamos para estilizar uma página da Web.
+    CSS significa Cascading Style Sheets (Folhas de Estilo em Cascata).
+    O CSS descreve como os elementos HTML devem ser exibidos na tela. papel, ou em outros meios
+    O CSS economiza muito trabalho. Ele pode controlar o layout de várias páginas da web simultaneamente
+    Folhas de estilo externas são armazenadas em arquivos CSS.
+
+    Por que usar CSS?
+    O CSS é usado para definir estilos para suas páginas da web, 
+    incluindo o design e o layout. e variações na exibição para diferentes dispositivos e tamanhos de tela.
+
+    Exemplo de CSS
+    body {
+    background-color: lightblue;
+    }
+
+    h1 {
+    color: white;
+    text-align: center;
+    }
+
+    p {
+    font-family: verdana;
+    font-size: 20px;
+    }
+`, "link da documentação: https://www.w3schools.com/css/css_intro.asp", "sem exercio disponivel"]
+}
